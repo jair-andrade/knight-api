ba057e28c7a9f8b58e082873756d770d2c90b4c7
jair-andrade <jair.andrad.filho@gmail.com>
2024-02-12 13:32:46 -0300

    feat: initial commit

---
+++ src/controllers/create-weapon.controller.ts
@@ -0,0 +1,38 @@
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+import { PrismaService } from "src/prisma/prisma.service";
+import { z } from "zod";
+
+const createKnightBodySchema = z.object({
+  name: z.string(),
+  nickname: z.string(),
+  birthday: z.date(),
+  attributes: z.string(),
+  key_attribute: z.string(),
+});
+
+const bodyValidationPipe = new ZodValidationPipe(createKnightBodySchema);
+
+type CreateKnightBodySchema = z.infer<typeof createKnightBodySchema>;
+
+@Controller("/weapons")
+@UseGuards(JwtAuthGuard)
+export class CreateKnightController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  async handle(@Body(bodyValidationPipe) body: CreateKnightBodySchema) {
+    const { name, nickname, birthday, attributes, key_attribute } = body;
+
+    await this.prisma.weapons.create({
+      data: {
+        name,
+        nickname,
+        birthday,
+        attributes,
+        key_attribute,
+      },
+    });
+  }
+}
